73d6d1f013be3ebc0c441c057b9bf7fadbf5613a
Tyler Torola <Tyler@TJT.Codes>
2016-03-11 15:19:11 -0800

    added JS structure and item-variants

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -3,11 +3,11 @@
   <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta id="csrf-token" content="{{ csrf_token() }}" />
 
     <title>Samwise</title>
     <!-- Tell the browser to be responsive to screen width -->
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+    <link rel="stylesheet" href="/css/vendor.min.css">
 
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -16,7 +16,11 @@
         <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
   </head>
-  <body>
-    Welcome to the admin panel.
+  <body class="hold-transition skin-blue sidebar-mini">
+    <div id="app"></div>
+
+    <!-- REQUIRED JS SCRIPTS -->
+    <script src="/js/vendor.js"></script>
+    <script src="/js/main.js"></script>
   </body>
 </html>
