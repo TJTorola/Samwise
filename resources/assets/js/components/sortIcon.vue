2133563dcf90d336cccd86b594d95bba4853f348
Tyler Torola <TJTorola@0.local>
2016-03-22 15:37:17 -0700

    added sorting controls to vue

---
+++ resources/assets/js/components/sortIcon.vue
@@ -0,0 +1,14 @@
+<template>
+	<i :class="(sort.key == key)?sort.ascending?'fa fa-sort-amount-asc':'fa fa-sort-amount-desc':''"></i>
+</template>
+<script>
+module.exports = {
+	props: ['key'],
+
+	vuex: {
+		getters: {
+			sort: state => state.inventory.sort
+		}
+	}
+}
+</script>
