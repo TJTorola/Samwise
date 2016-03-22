524feae8f7b50d0e2178faf10ce949f889b213a2
Tyler Torola <Tyler@TJT.Codes>
2016-03-22 10:59:48 -0700

    finished all on inventoryTable except sorting and limit

--- resources/assets/js/vuex/modules/lucky_sevens.rb
+++
@@ -1,8 +0,0 @@
-def lucky_sevens(array)
-	return true
-end
-
-puts(lucky_sevens?([2,1,5,1,0]) == true).to_s
-puts(lucky_sevens?([0,-2,1,8]) == true).to_s
-puts(lucky_sevens?([7,7,7,7]) == false).to_s
-puts(lucky_sevens?([3,4,3,4]) == false).to_s
