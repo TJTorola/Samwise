2a1e6c082400ed8ac355c39ff2292fe4523e38a2
vagrant <vagrant@samwise>
2016-03-23 04:28:34 +0000

    started add-to-cart scaffolding

---
+++ resources/assets/js/components/addToCart.vue
@@ -0,0 +1,39 @@
+<template>
+	<button type="button" class="btn btn-block btn-warning btn-xs u-no-margin" 
+		@click="incrament" 
+		@contextmenu.prevent="decrament">
+		{{ (count)?count:'+' }} <i class="fa fa-shopping-cart"></i>
+	</button>	
+</template>
+
+<script>
+module.exports = {
+	data () {
+		return {
+			count: this.cart[this.id]
+		}
+	},
+
+	props: ['id'],
+
+	methods: {
+		incrament() {
+			this.addToCart(this.id)
+			this.count = this.cart[this.id]
+		},
+
+		decrament() {
+			this.removeFromCart(this.id)
+			this.count = this.cart[this.id]
+		}
+	},
+
+	vuex: {
+		getters: {
+			cart: state => state.cart.items
+		},
+
+		actions: require('../vuex/actions/cart.js')
+	}
+}
+</script>
--- resources/assets/js/vuex/modules/cart.js
+++ resources/assets/js/vuex/modules/cart.js
@@ -1,8 +1,26 @@
 const state = {
-	items: []
+	items: {},
+	count: 0
 }
 
-const mutations = {}
+const mutations = {
+	ADD_TO_CART (state, variantId) {
+		if (state.items[variantId]) {
+			state.items[variantId]++
+			state.count++
+		} else {
+			state.items[variantId] = 1
+			state.count++
+		}
+	},
+
+	REMOVE_FROM_CART (state, variantId) {
+		if (state.items[variantId] && state.items[variantId] > 0) {
+			state.items[variantId] -= 1
+			state.count--
+		}
+	}
+}
 
 module.exports = {
 	state,
