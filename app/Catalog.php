1aeb06bdffb5fd3cd96706606aa9934cc206725b
vagrant <vagrant@samwise>
2016-03-31 22:40:50 +0000

    re-implamented catalog.vue

--- app/Catalog.php
+++ app/Catalog.php
@@ -7,47 +7,49 @@ use Illuminate\Database\Eloquent\Model;
 class Catalog extends Model
 {
 	/**
-   * The accessors to append to the model's array form.
-   *
-   * @var array
-   */
-  protected $appends = [];
+	 * The accessors to append to the model's array form.
+	 *
+	 * @var array
+	 */
+	protected $appends = [];
 
-  /**
-   * The attributes that should be casted to native types.
-   *
-   * @var array
-   */
-  protected $casts = [];
+	/**
+	 * The attributes that should be casted to native types.
+	 *
+	 * @var array
+	 */
+	protected $casts = [
+		'label' => 'boolean'
+	];
 
-  /**
-   * The attributes that aren't mass assignable.
-   *
-   * @var array
-   */
-  protected $guarded = ['id', 'created_at', 'updated_at', 'prev', 'next'];
+	/**
+	 * The attributes that aren't mass assignable.
+	 *
+	 * @var array
+	 */
+	protected $guarded = ['id', 'created_at', 'updated_at', 'prev', 'next'];
 
-  /**
-   * Retrieve the prev item
-   */
-  public function getPrevAttribute()
-  {
-    $prev_id = Catalog::where('id', '<', $this->id)->max('id');
-    if ($prev_id == null) {
-      $prev_id = Catalog::all()->max('id');
-    }
-    return "/catalog/$prev_id";
-  }
+	/**
+	 * Retrieve the prev item
+	 */
+	public function getPrevAttribute()
+	{
+		$prev_id = Catalog::where('id', '<', $this->id)->max('id');
+		if ($prev_id == null) {
+			$prev_id = Catalog::all()->max('id');
+		}
+		return "/catalog/$prev_id";
+	}
 
-  /**
-   * Retrieve the next item
-   */
-  public function getNextAttribute()
-  {
-    $next_id = Catalog::where('id', '>', $this->id)->min('id');
-    if ($next_id == null) {
-      $next_id = Catalog::all()->min('id');
-    }
-    return "/catalog/$next_id";
-  }
+	/**
+	 * Retrieve the next item
+	 */
+	public function getNextAttribute()
+	{
+		$next_id = Catalog::where('id', '>', $this->id)->min('id');
+		if ($next_id == null) {
+			$next_id = Catalog::all()->min('id');
+		}
+		return "/catalog/$next_id";
+	}
 }
---
+++ resources/assets/js/components/tags.vue
@@ -0,0 +1,77 @@
+<template>
+<div class='box box-warning'>
+	<div class='box-header'>
+		<h3 class='box-title'>Tags</h3>
+
+		<div class="box-tools pull-right">
+			<button class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i></button>
+		</div>
+	</div>
+
+	<div class="box-body">
+		<div class="col-xs-12">
+			<div class="tag" v-for="tag in tags" @click="tags.splice($index, 1)">{{ tag }}</div>
+			<div class="tag is-new" v-if="newTag" @click="newTag=''">{{ newTag }}</div>
+			<hr v-if="tags.length > 0">
+			<div class="input-group">
+        <input type="text" class="form-control" v-model="newTag" @keyup.enter="addTag" list="tagList">
+        <datalist id="tagList">
+        	<option value="{{ tag }}" v-for="tag in tagList"></option>
+        </datalist>
+        <span class="input-group-btn">
+          <button class="btn btn-primary" type="button" @click="addTag"><i class="fa fa-plus"></i> Add New Tag</button>
+        </span>
+      </div>
+      <br>
+		</div>
+	</div>
+</div>
+</template>
+
+<script>
+module.exports = {
+	data () {
+		if (this.tagString.length) {
+			 var tags = this.tagString.split(',')
+		} else {
+			 var tags = []
+		}
+
+		this.$http.get('/info/tags').then(function(response) {
+			this.$set('tagList', response.data)
+		})
+
+		return {
+			tags: tags,
+			newTag: '',
+			tagList: [],
+		}
+	},
+
+	props: ['tagString'],
+
+	watch: {
+		tagString () {
+			if (this.tagString.length) {
+				this.tags = this.tagString.split(',')
+			} else {
+				this.tags = []
+			}
+		},
+
+		tags () {
+			this.tagString = this.tags.join(',')
+		}
+	},
+
+	methods: {
+		addTag: function() {
+			if ($.inArray(this.newTag, this.tags) == -1 && this.newTag != '') {
+				this.tags.push(this.newTag)
+			}
+
+			this.newTag = ''
+		},
+	}
+}
+</script>
