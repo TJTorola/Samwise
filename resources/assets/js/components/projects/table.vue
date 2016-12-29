04b0d51d433970b10fbb5ddad1e6524ee3dd8fa8
Tyler Torola <tyler@tjt.codes>
2016-12-28 17:08:19 -0800

    builds fix

---
+++ resources/assets/js/components/projects/table.vue
@@ -0,0 +1,32 @@
+<template>
+	<table class="table table-hover">
+		<thead is="sorters"></thead>
+
+		<tbody v-for="invoice in invoices">
+			<tr is="row" :invoice="invoice"></tr>
+			<tr v-if="expandedIndex == invoice.id" class="hidden-xxs" style="background: white;">
+				<td colspan="7">
+					<summary :invoice="invoice"></summary>
+				</td>
+			</tr>
+		</tbody>
+	</table>
+</template>
+
+<script>
+module.exports = {
+	props: ['invoices'],
+
+	components: {
+		summary: require('app/components/invoice/summary.vue'),
+		sorters: require('./sorters.vue'),
+		row: require('./row.vue')
+	},
+
+	vuex: {
+		getters: {
+			expandedIndex: state => state.invoices.expandedIndex
+		}
+	}
+}
+</script>
